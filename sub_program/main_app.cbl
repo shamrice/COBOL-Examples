541a492699278e5de456ce5f4c1fa72218c988a9
shamrice <shamrice@gmail.com>
2022-04-13 00:53:16 -0400

    Expanded upon the sub program example a bit showing how working-storage retains values in called sub program until cancelled.

--- sub_program/main_app.cbl
+++ sub_program/main_app.cbl
@@ -1,6 +1,7 @@
       ******************************************************************
       * Author: Erik Eriksen
       * Date: 2020-04-16
+      * Updated: 2022-04-13
       * Purpose: Main application calls sub-app by content and reference
       * Tectonics: cobc -x main_app.cbl sub.cbl -o a.out
       ******************************************************************
@@ -19,30 +20,53 @@
 
        procedure division.
        main-procedure.
-           display "Enter value for #1: "
+           display space
+           display "Enter value for #1: " with no advancing
            accept ws-item-1
 
-           display "Enter value for #2: "
+           display "Enter value for #2: " with no advancing
            accept ws-item-2.
 
            perform display-message
 
-           display "Calling subroutine by content:"
+      *> Calling by content, the variables passed will not be modified
+      *> upon return to the main application.
+           display "Calling sub program by content:"
            call "sub-app" using
-               by content ws-item-1,
+               by content ws-item-1
                by content ws-item-2
            end-call
-
            perform display-message
-           display "Calling subroutine by reference:"
-           call "sub-app" using ws-item-1, ws-item-2
 
+      *> Calling by reference (default) the variables can be modified by
+      *> the called sub program. Note that the working-storage variables
+      *> of the sub program retain their values between calls where the
+      *> linkage section variables do not.
+           display "Second call of sub program should retain WS values."
+           display "Calling sub program by reference:"
+           call "sub-app" using
+               ws-item-1 ws-item-2
+           end-call
            perform display-message
+
+      *> Cancelling the sub program will reset all variables in the
+      *> working storage section back to their original values.
+           display "Cancelling sub program"
+           cancel "sub-app"
+           display "Calling sub program. WS values should be reset:"
+           call "sub-app" using
+               ws-item-1 ws-item-2
+           end-call
+           perform display-message
+
 
            stop run.
 
        display-message.
-           display "Main app: " ws-group-1.
+           display space
+           display "-----------------------------------------------"
+           display "Main app: " ws-group-1
+           exit paragraph.
 
 
        end program main-app.
--- sub_program/sub.cbl
+++ sub_program/sub.cbl
@@ -1,6 +1,7 @@
       ******************************************************************
       * Author: Erik Eriksen
       * Date: 2021-04-16
+      * Updated: 2022-04-13
       * Purpose: Sub program called by main program.
       * Tectonics: cobc -x main_app.cbl sub.cbl -o a.out
       ******************************************************************
@@ -10,21 +11,57 @@
        data division.
 
        file section.
+
+      *> Working storage values persist until a "cancel" call is made
+      *> on the sub program.
        working-storage section.
+       01  ws-test-item-1                 pic x(10).
+       01  ws-test-item-2                 pic x(10).
+
+      *> Local storage values are fresh on each call of the sub program
+      *> even if no "cancel" statement is made.
+       local-storage section.
+       01  ls-test-item-1                 pic x(10).
+       01  ls-test-item-2                 pic x(10).
 
        linkage section.
-           01 ls-test-item-1               pic x(10).
-           01 ls-test-item-2               pic x(10).
+       01  l-test-item-1                  pic x(10).
+       01  l-test-item-2                  pic x(10).
 
-       procedure division using ls-test-item-1, ls-test-item-2.
+       procedure division using l-test-item-1 l-test-item-2.
        main-procedure.
-           display "In sub program: " ls-test-item-1 " " ls-test-item-2
+           display "In sub program: " l-test-item-1 " " l-test-item-2
+           display space
+           display "working-storage values at start:"
+           display "ws-test-item-1: " ws-test-item-1
+           display "ws-test-item-2: " ws-test-item-2
+           display space
+           display "local-storage values at start:"
+           display "ls-test-item-1: " ls-test-item-1
+           display "ls-test-item-2: " ls-test-item-2
+           display space
+           display "Moving linkage section values to ws and ls vars.."
 
-           display "setting variables to new value..."
-           move "replace1" to ls-test-item-1
-           move "replace2" to ls-test-item-2
+           move l-test-item-1 to ws-test-item-1
+           move l-test-item-2 to ws-test-item-2
+           move l-test-item-1 to ls-test-item-1
+           move l-test-item-2 to ls-test-item-2
 
-           display "In sub program: " ls-test-item-1 " " ls-test-item-2
+
+           display "setting input variables to new value..."
+           move "replace1" to l-test-item-1
+           move "replace2" to l-test-item-2
+
+           display space
+           display "working-storage values at end:"
+           display "ws-test-item-1: " ws-test-item-1
+           display "ws-test-item-2: " ws-test-item-2
+           display space
+           display "local-storage values at end:"
+           display "ls-test-item-1: " ls-test-item-1
+           display "ls-test-item-2: " ls-test-item-2
+           display space
+           display "Exit sub program: " l-test-item-1 " " l-test-item-2
            goback.
 
        end program sub-app.
